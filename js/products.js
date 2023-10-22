a27f89bd71c5d700a79bf58ee0b97453dd018fb6
Patricia Ruiz Diaz <91780371+perd2020@users.noreply.github.com>
2023-10-22 00:43:21 -0300

    subi el codigo

---
+++ js/products.js
@@ -0,0 +1,68 @@
+
+const productos = [
+
+    {
+        id: 1,
+        nombre: "topper roja/azul",
+        precio: 200,
+        img: "/img/1.png",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre: "lacoste azul/verde",
+        precio: 300,
+        img: "/img/2.png",
+        cantidad: 1,
+    },
+    {
+        id: 3,
+        nombre: "topper  botitas azul/naranja/celeste",
+        precio: 200,
+        img: "/img/3.png",
+        cantidad: 1,
+    },
+    {
+        id: 4,
+        nombre: "topper botitas celeste/azul",
+        precio: 200,
+        img: "/img/4.png",
+        cantidad: 1,
+    },
+    {
+        id: 5,
+        nombre: "assics roja/negra",
+        precio: 500,
+        img: "/img/5.png",
+        cantidad: 1,
+    },
+    {
+        id: 6,
+        nombre: "salomon celeste/blanco",
+        precio: 1000,
+        img: "/img/6.png",
+        cantidad: 1,
+    },
+    {
+        id: 7,
+        nombre: "adidas verde/naranja",
+        precio: 700,
+        img: "/img/7.png",
+        cantidad: 1,
+    },
+    {
+        id: 8,
+        nombre: "topper roja/azul7amarilla",
+        precio: 200,
+        img: "/img/8.png",
+        cantidad: 1,
+    },
+    {
+        id: 9,
+        nombre: "nike celeste/blanca",
+        precio: 200,
+        img: "/img/9.png",
+        cantidad: 1,
+    },
+    
+];
